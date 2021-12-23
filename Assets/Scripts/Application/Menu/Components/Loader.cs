ab511ccc6260b063db9e2c33311ae4a0bfe5a156
Yonk Todorcevic <yonk_cs@hotmail.com>
2021-12-23 15:39:39 +0100

    Load sprites by addressables done

--- Assets/Scripts/Application/Menu/Components/Loader.cs
+++ Assets/Scripts/Application/Menu/Components/Loader.cs
@@ -9,11 +9,13 @@ namespace Arkham.Application
         [Inject] private readonly ScreenResolutionAutoDetect resolutionSetter;
         [Inject] private readonly CardFactory cardFactory;
         [Inject] private readonly DataContext dataContext;
+        [Inject] private readonly ImagesCardManager imagesCardManager;
 
         private void Awake()
         {
             resolutionSetter.SettingResolution();
             dataContext.LoadDataCards();
+            imagesCardManager.Load();
             cardFactory.BuildDeckCards();
             cardFactory.BuildInvestigatorCards();
             //mapper.SaveProgress();
